b9e43796d1cf8c7ae0e810c0e7b63ffabaf9e6b5
Hrvoje Herak <hrvoje.herak@gmail.com>
2019-03-09 19:21:32 +0100

    ipak sam promijenio port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 
 app.patch('/', (req, res) => res.send('PATCH endpoint'))
 app.post('/', (req, res) => res.send('POST endpoint'))
