cfdad1d91c39a4643ca2f7c6cf61a614a79a8e55
Hrvoje Herak <hrvoje.herak@gmail.com>
2019-03-09 19:06:41 +0100

    promijenio sam port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.patch('/', (req, res) => res.send('PATCH endpoint'))
 app.post('/', (req, res) => res.send('POST endpoint'))
